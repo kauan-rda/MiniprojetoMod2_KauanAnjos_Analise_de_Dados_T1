b502e445cfa21a671ae77397db84b5ccf2da33ff
kauan-rda <kauan.anjos@catolicasc.edu.br>
2026-09-14 21:11:43 -0300

    Update 01-carga-staging.sql
    Feat: explicando pequeno fix devido uso de supabase

--- 01-carga-staging.sql
+++ 01-carga-staging.sql
@@ -1,4 +1,4 @@
--- Retirado parte de create database por utilizar supabase, sendo desenecessario
+-- Retirado parte de create database por utilizar supabase, ja que o banco e vinculado com a abertura do projeto
 -- ----------------------------------------------------------------------------------
 -- stg_pedido  (4044 linhas)
 -- ----------------------------------------------------------------------------------
